f67007d4ac4f6dfe463e0816927fffcf3f0cfe6d
Tyler Vegh <tyler@vegh.tv>
2020-06-27 16:19:18 -0400

    Add files via upload

---
+++ sort-tour.py
@@ -0,0 +1,53 @@
+import os
+import Tkinter
+import tkFileDialog
+import subprocess
+from lxml import etree
+
+root = Tkinter.Tk()
+
+
+def findFile():
+    # ask user to select project directory
+    global fileDir
+    global mainDir
+    mainDir = tkFileDialog.askdirectory(
+        parent=root, initialdir="/", title='Please select the main directory')
+    # save .p2vr file directory
+    fileDir = tkFileDialog.askopenfilename(
+        parent=root, initialdir=mainDir, title='Please select the p2vr file')
+    print('Found the .p2vr file')
+    # change directory to project directory
+    os.chdir(mainDir)
+
+
+# this takes in a hotspot XML element, then checks if there is a position/tilt element
+# if there is one, return that value
+# if there isnt one, it returns the next/tilt value
+# this is here because polygon hotspots do not have position/tilt values
+# which would cause the program not to work.
+def hotspotTilt(hs):
+    if (hs.find('./position/tilt') != None):
+        return float(hs.find('./position/tilt').text)
+    else:
+        return float(hs.find('./next/tilt').text)
+
+
+def sort():
+    # Read the .p2vr file in
+    with open(fileDir) as in_file:
+        xml = etree.parse(in_file)
+        xmlRoot = xml.getroot()
+    # change the root to 'hotspots'
+    xmlRoot = xmlRoot[0][0][5]
+
+    # sort the data according tilt value
+    xmlRoot[:] = sorted(xmlRoot[:], key=hotspotTilt)
+
+    # Write the file out
+    with open('sorted.p2vr', 'w') as out_file:
+        xml.write(out_file)
+
+
+findFile()
+sort()
